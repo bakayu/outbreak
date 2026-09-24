22d70ebe353f7fdf45b77a42ee18a30472c63b15
Ayush <mail@ayuch.dev>
2026-09-24 21:50:02 +0530

    wip: save best 50 scores in localStorage
    Signed-off-by: Ayush <mail@ayuch.dev>

--- js/config.js
+++ js/config.js
@@ -6,17 +6,20 @@ export const DIFFICULTY = {
 		zombieHp: 15,
 		spawnInterval: 120,
 		label: "Easy",
+		weight: 1,
 	},
 	medium: {
 		zombieSpeed: 2.7,
 		zombieHp: 30,
 		spawnInterval: 90,
 		label: "Medium",
+		weight: 2,
 	},
 	hard: {
 		zombieSpeed: 3.6,
 		zombieHp: 45,
 		spawnInterval: 60,
 		label: "Hard",
+		weight: 3,
 	},
 };
